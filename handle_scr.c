b15f4743dc06ebf99f7153dc5c8782571d627135
Szero <singleton@tfwno.gf>
2018-02-26 20:14:12 +0100

    proper operators and indentation

--- handle_scr.c
+++ handle_scr.c
@@ -19,22 +19,22 @@ void init_screen(void)
 void colors(void)
 {
 
-        if(has_colors() == FALSE) {
-            endwin();
-            perror("Your terminal doesn\'t even have 50 shades of gray ...\n");
-            exit(1);
-        }
-        use_default_colors();
-        start_color();
-        init_pair(1, -1, -1);
-        init_pair(2, COLOR_BLACK, -1);
-        init_pair(3, COLOR_RED, -1);
-        init_pair(4, COLOR_GREEN, -1);
-        init_pair(5, COLOR_YELLOW, -1);
-        init_pair(6, COLOR_BLUE, -1);
-        init_pair(7, COLOR_MAGENTA, -1);
-        init_pair(8, COLOR_CYAN, -1);
-        init_pair(9, COLOR_WHITE, -1);
+    if(has_colors() == FALSE) {
+        endwin();
+        perror("Your terminal doesn\'t even have 50 shades of gray ...\n");
+        exit(1);
+    }
+    use_default_colors();
+    start_color();
+    init_pair(1, -1, -1);
+    init_pair(2, COLOR_BLACK, -1);
+    init_pair(3, COLOR_RED, -1);
+    init_pair(4, COLOR_GREEN, -1);
+    init_pair(5, COLOR_YELLOW, -1);
+    init_pair(6, COLOR_BLUE, -1);
+    init_pair(7, COLOR_MAGENTA, -1);
+    init_pair(8, COLOR_CYAN, -1);
+    init_pair(9, COLOR_WHITE, -1);
 
 }
 
@@ -46,13 +46,13 @@ void draw_bar(unsigned char posX,unsigned char posY, int box_nr, char pair)
     y2 = posY+1;
     x2 = posX+2;
     unsigned char i;
-    for (i = 0; i < box_nr*2; i=i+2) {
+    for (i = 0; i < box_nr*2; i+=2) {
         if (i == 0) {
-            y1=y1+i;
-            y2=y2+i;
+            y1 += i;
+            y2 += i;
         } else {
-            y1 = y1+2;
-            y2 = y2+2;
+            y1 += 2;
+            y2 += 2;
         }
         wattron(win, COLOR_PAIR(pair));
         mvwhline(win, y1, x1, 0, x2-x1);
@@ -70,8 +70,7 @@ void draw_bar(unsigned char posX,unsigned char posY, int box_nr, char pair)
 
 void draw_time(unsigned char posX, unsigned char posY, char bar, char bar_size, char pair)
 {
-    char i,pos;
-    pos =0;
+    char i = 0, pos = 0;
 
     for(i=bar_size-1; i>=0; i--) {
         if (bar&(1<<(bar_size-1-i))){
@@ -84,7 +83,7 @@ void draw_time(unsigned char posX, unsigned char posY, char bar, char bar_size, 
             mvwaddch(win,posY+6-pos,posX+1,ACS_HLINE);
             wattroff(win,COLOR_PAIR(pair));
         }
-            pos = pos + 2;
+            pos += 2;
     }
 }
 
